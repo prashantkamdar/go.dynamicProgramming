99362ee019dddd1af9138efae4d90eb772d0a738
prashant <prashantkamdar@gmail.com>
2021-05-03 21:24:26 +0530

    grid traveller

---
+++ gridTraveller/gridTraveller.go
@@ -0,0 +1,25 @@
+package gridTraveller
+
+import "strconv"
+
+func CalcPaths(m int, n int, x ...*map[string]int) int {
+
+	paths := make(map[string]int)
+
+	if len(x) != 0 {
+		paths = *x[0]
+	}
+	if val, ok := paths[strconv.Itoa(m)+"+"+strconv.Itoa(n)]; ok {
+		//fmt.Println(val)
+		return val
+	}
+
+	if m == 0 || n == 0 {
+		return 0
+	}
+	if m == 1 && n == 1 {
+		return 1
+	}
+	paths[strconv.Itoa(m)+"+"+strconv.Itoa(n)] = CalcPaths(m-1, n, &paths) + CalcPaths(m, n-1, &paths)
+	return paths[strconv.Itoa(m)+"+"+strconv.Itoa(n)]
+}
--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 
 	"github.com/prashantkamdar/go.dynamicProgramming/fibonacci"
+	"github.com/prashantkamdar/go.dynamicProgramming/gridTraveller"
 )
 
 func main() {
 
 	fmt.Println(fibonacci.CalcFib(50))
+
+	fmt.Println(gridTraveller.CalcPaths(18, 18))
 }
